e3683a7a97bef96d36b4071d1fb4525a6a7a983a
Vikram Kangotra <vikramkangotra8055@gmail.com>
2023-09-03 00:04:17 +0530

    deleted unnecessary function

--- main.cpp
+++ main.cpp
@@ -1,11 +1,5 @@
 #include <emscripten.h>
 
-extern "C" void run(const char* code) {
-    MAIN_THREAD_EM_ASM({
-        eval(UTF8ToString($0));
-    }, code);
-}
-
 extern "C" {
     extern void runthis();
 }
