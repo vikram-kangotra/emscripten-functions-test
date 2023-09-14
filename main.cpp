83d02dc5992a039f2a86f8fac90934e1deb68938
Vikram Kangotra <vikramkangotra8055@gmail.com>
2023-09-14 19:22:50 +0530

    WIP: Refactoring

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <emscripten.h>
+#include <iostream>
 
 extern "C" {
     extern void runthis();
 }
 
 int main() {
+
+    const char* x = CODE_EXPR("console.log('Hello World!');");
+
+    std::cout << (void*)x << std::endl;
+
+    const char* ok = "console.log('Hello World!');";
+
+    std::cout << (void*)ok << std::endl;
 
     runthis();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,49 @@ pub extern "C" fn handle_video_frame(buffer: *const i8, width: i32, height: i32)
     println!("width: {}, height: {}", width, height);
 }
 
+macro_rules! CODE_EXPR {
+    ($code:block) => {
+        {
+            const C: &[u8] = stringify!($code).as_bytes();
+            const C_LEN: usize = C.len();
+
+            const BUF_LEN: usize = C_LEN + 1;
+
+            #[used]
+            #[link_section = "em_asm"]
+            static BUF: [u8; BUF_LEN] = {
+                let mut arr = [0u8; BUF_LEN];
+                let mut idx = 0;
+                while idx < C_LEN {
+                    arr[idx] = C[idx];
+                    idx += 1;
+                }
+                arr[idx] = b'\0';
+                arr
+            };
+
+            BUF.as_ptr() as *const i8
+        }
+    };
+}
+
+macro_rules! MAIN_THREAD_EM_ASM {
+    ($code:block) => {
+        unsafe {
+            emscripten_functions_sys::emscripten::
+            emscripten_asm_const_int_sync_on_main_thread(CODE_EXPR!($code), "\0".as_ptr() as *const i8);
+        }
+    };
+}
+
 #[no_mangle]
 fn runthis() {
 
+    MAIN_THREAD_EM_ASM!({
+        const mediaStream = navigator.mediaDevices.getUserMedia({ video: true });
+    });
+
+    /*
     let mut buffer = vec![0 as i8; 640 * 480 * 4];
 
     for i in 0..buffer.len() {
@@ -60,4 +100,5 @@ fn runthis() {
         emscripten_functions_sys::emscripten::
         emscripten_asm_const_int_sync_on_main_thread(CODE.as_ptr() as *const i8, "i\0".as_ptr() as *const i8, buffer.as_mut_ptr() as *const i8);
     }
+    */
 }
